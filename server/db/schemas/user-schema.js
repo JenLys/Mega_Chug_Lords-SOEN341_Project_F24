ffc46a46058b24fa53d473e8a1ddcbfa40d161f9
Sasha Klein-Charland <sp.kleincharland@gmail.com>
2024-09-26 10:40:54 -0400

    Add express route for student login and mongoose schema files

---
+++ server/db/schemas/user-schema.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        fname: String,
+        lname: String,
+        role: String,
+        user_id: String,
+        pw: String
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,27 @@
-import express from "express";
+import express, { urlencoded } from "express";
 // import db from "../db/connection.js"
 import { ObjectId } from "mongodb";
 
 const router = express.Router();
+router.use(express.json());
+router.use(urlencoded());
+
 router.get("/hello", (_, res) => {
   res.json({ "hello": "world" })
+})
+
+router.get("/login/student", (req, res) => {
+  console.log("Student login page");
+  res.end();
+})
+
+router.post("/login/student", (req, res) => {
+  const id = req.body.id || "";
+  const pw = req.body.pw || "";
+  console.log(userData);
+  
+  // Redirect to login page with a successful signup popup message
+  return(res.redirect("/login")); 
 })
 
 export default router;
