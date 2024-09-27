b0aa50b85c1f60f84ba34813cd0b98c0b3426596
Marian-Hristov <marianhr2014@gmail.com>
2024-09-27 16:04:44 -0400

    Added mongoose instead of mongodb as db client

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,5 @@
 import express from "express";
-// import db from "../db/connection.js"
-import { ObjectId } from "mongodb";
+import db from "../db/connection.js"
 
 const router = express.Router();
 router.get("/hello", (_, res) => {
