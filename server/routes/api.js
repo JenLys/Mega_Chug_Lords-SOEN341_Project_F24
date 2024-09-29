c1b367acda478dcd6515b5eb1b2353e1ce6d08c5
Sasha Klein-Charland <sp.kleincharland@gmail.com>
2024-09-29 16:43:43 -0400

    Add function for user login route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,13 @@
 import express from "express";
 import db from "../db/connection.js";
-import User from "../schemas/user-schema.js";
+import User from "../db/schemas/user.js";
+
+import studentLogin from "./studentLogin.js"
 
 const router = express.Router();
 router.use(express.json());
-router.use(urlencoded());
+router.use(express.urlencoded({extended:true}));
+
 
 router.get("/hello", (_, res) => {
   res.json({ hello: "world" });
@@ -15,13 +18,28 @@ router.get("/login/student", (req, res) => {
   res.end();
 })
 
-router.post("/login/student", (req, res) => {
-  const id = req.body.id || "";
-  const pw = req.body.pw || "";
-  console.log(userData);
+/* router.post("/login/student", async (req, res) => {
+  const dbUser = await db.getUser(req.body.user_id);
+
+  if (!dbUser) // dbUser is null, does not exist in db
+  {
+    res.sendStatus(401);
+  } 
+  
+  else
+  {
+    if (req.body.pw !== dbUser.pw) // Passwords do not match
+    {
+      console.log("Incorrect password.")
+      res.sendStatus(401);
+    } 
+  }
+
+  res.sendStatus(200) // Login successful
   
   // Redirect to login page with a successful signup popup message
-  return(res.redirect("/login")); 
-})
+}) */
+
+router.post("/login/student", studentLogin);
 
 export default router;
