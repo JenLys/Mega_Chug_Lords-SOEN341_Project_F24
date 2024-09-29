86b542a075f4589ad19ba48d48b4f1674d698438
vinuyans <vinuyansivakolunthu@gmail.com>
2024-09-28 21:41:33 -0400

    added function to get all objects from collection

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,11 @@
 import express from "express";
-import db from "../db/connection.js"
+import db from "../db/connection.js";
+import User from "../schemas/user-schema.js";
 
 const router = express.Router();
 router.get("/hello", (_, res) => {
-  res.json({ "hello": "world" })
-})
+  res.json({ hello: "world" });
+});
 
+console.log(await db.getAll(User));
 export default router;
