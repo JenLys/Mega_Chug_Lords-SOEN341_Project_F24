5ba78b69177a5472b486e30d6e6fd08ce9e5b93a
Jeslyn <jeslyn.sorotsky@gmail.com>
2024-09-29 00:37:07 -0400

    updated the extra features About and HowToUse

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <h1>About ReviewMate</h1>
+    <div style={{ display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      color: 'rgb(0,71,115)', }}>
+      <h1 style={{ fontSize: '2.5rem', fontWeight: 'bold' }}>About ReviewMate</h1>
       <p>In construction</p>
     </div>
   );
