59ab4f14a1c1eb356652c45a68a92eec36e45674
vinuyans <vinuyansivakolunthu@gmail.com>
2024-09-28 21:41:33 -0400

    added function to get all objects from collection

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const uri = process.env.ATLAS_URI || "";
-const dbName = process.env.DB_NAME || "reviewmate"
+const dbName = process.env.DB_NAME || "reviewmate";
 
 if (uri.length === 0) {
-  process.exit(-1)
+  process.exit(-1);
 }
 
 class Db {
@@ -15,12 +15,18 @@ class Db {
   connectDb() {
     let dbOptions = {
       dbName: "" + dbName,
-    }
-    mongoose.connect(uri, dbOptions)
+    };
+    mongoose.connect(uri, dbOptions);
+    console.log("Connected to MongoDB");
   }
 
   disconnectDb() {
-    mongoose.connection.close()
+    mongoose.connection.close();
+  }
+
+  async getAll(collectionSchema) {
+    const collection = collectionSchema.find();
+    return collection;
   }
 }
 
