462bbf12c4afbc8c94b0fc7820f8736c9c1954aa
vinuyans <vinuyansivakolunthu@gmail.com>
2024-10-31 11:19:37 -0400

    made component for teacher course view

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,8 @@
+import TeacherCourseDetail from "./TeacherCourseDetail";
 function About() {
   return (
     <div>
+      <TeacherCourseDetail course={{course_id: "67210c7f8ee3047e8766f0e4"}}/>
       <h1>About ReviewMate</h1>
       <p>In construction</p>
     </div>
