e8bdf310fca1ba8a3e7613f77b4a769a8b06b201
Sasha Klein-Charland <sp.kleincharland@gmail.com>
2024-10-31 16:54:23 -0400

    add comments and formatting

--- server/__tests__/acceptanceTestUS01.test.js
+++ server/__tests__/acceptanceTestUS01.test.js
@@ -1,16 +1,18 @@
-import { Db } from "../db/connection";
+import { Db } from "../db/connection.js";
 
+// Used since there is a student object in the test DB with these credentials
 const id = "12345679";
 const pw = "Password!2";
 
 let db;
 
 describe("Student login tests", () => {
+    // We only want to open the DB connection once
     beforeAll( () => {
-        db = new Db(true);
+        // 'true' opens connection to test db instead of main db; see connection.js
+        db = new Db(true);  
         return db;
     });
-
     afterAll( () => {
         try {
             db.disconnectDb();
@@ -19,21 +21,11 @@ describe("Student login tests", () => {
         }
     });
 
-    test('returns true if the db returns an object', async () => {
+    test('returns true if the db returns a non-null object', async () => {
         const data = await db.getUserLogin(id, pw);
         expect(data).toBeTruthy();
     });
-
-    test('returns true if the db returns the correct student object', async () => {
-        const data = await db.getUserLogin(id, pw);
-
-        expect(data).toHaveProperty('fname', 'Dent');
-        expect(data).toHaveProperty('lname', 'Stue');
-        expect(data).toHaveProperty('role', 'student');
-        expect(data).toHaveProperty('user_id', '12345679');
-        expect(data).toHaveProperty('pw', 'Password!2');
-    });
-
+    
     test('rejects a login attempt with an invalid username', async () => {
         const data = await db.getUserLogin('mimimi', pw);
         expect(data).toBeFalsy();
@@ -44,5 +36,13 @@ describe("Student login tests", () => {
         expect(data).toBeFalsy();
     });
 
+    test('returns true if the db returns the correct student object', async () => {
+        const data = await db.getUserLogin(id, pw);
     
-})
+        expect(data).toHaveProperty('fname', 'Dent');
+        expect(data).toHaveProperty('lname', 'Stue');
+        expect(data).toHaveProperty('role', 'student');
+        expect(data).toHaveProperty('user_id', '12345679');
+        expect(data).toHaveProperty('pw', 'Password!2');
+    });
+});
