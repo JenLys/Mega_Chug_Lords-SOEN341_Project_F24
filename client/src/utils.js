54e4d8216cf72f3227b7f083bbd2b7e513932a14
Marian-Hristov <marianhr2014@gmail.com>
2024-10-31 18:02:19 -0400

    Add comments to client utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Checks if the given object is null or undefined with === for both
+ * @param {any} value
+ * @return {Boolean}
+ */
 function isNull(value) {
   return value === null || value === undefined;
 };
@@ -6,6 +11,17 @@ const URL = import.meta.env.PROD_URL || "http://localhost:"
 const baseUrl = URL + PORT + "/api"
 const supportedMethods = ["GET", "POST"]
 
+/**
+ * Wrapper for doing requests using fetch, simplifies call and removes code repetition
+ * "URL:POST/api" will be prepended to the endpoint. The given method will be used. Only 
+ * GET, POST are supported as of now. The data will be passed according to the standards
+ * of the method
+ * @export
+ * @param {String} endpoint endpoint i
+ * @param {String} method GET, POST
+ * @param {Object} data data to be passed in the request following its standards/limitations
+ * @return {Promise<Object>} response 
+ */
 export async function request(endpoint, method, data) {
   if (!isNull(endpoint) && !isNull(method) && supportedMethods.includes(method)) {
     if (typeof endpoint === "string" && endpoint.startsWith("/")) {
