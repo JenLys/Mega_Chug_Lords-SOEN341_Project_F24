e3eef2c7e52894a27c0a150b5bef609399a82c01
Jeslyn <jeslyn.sorotsky@gmail.com>
2024-11-11 00:21:17 -0500

    created a page for the student's team view- change to modal later

---
+++ client/src/pages/MyTeam.jsx
@@ -0,0 +1,18 @@
+//WILL HAVE TO PUT THIS INTO A MODAL: IT WILL NOT STAY AS A PAGE 
+
+//TOLD TO LIMIT CSS FILES, ALL CSS WILL BE INLINE
+//WILL ALSO CREATE A ROUTE FOR THIS PAGE ONLY TO SEE HOW IT LOOKS VISUALLY---I WILL REMOVE IT AFTER
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation} from "react-router-dom";
+
+function MyTeam() {
+
+
+
+
+
+}
+
+export default MyTeam;
