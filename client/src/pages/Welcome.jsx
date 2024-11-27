ffe966b6cd724ff89af146b0e0db5bc9e367c88b
Jeslyn <jeslyn.sorotsky@gmail.com>
2024-11-26 20:02:50 -0500

    fixing button ui

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -24,18 +24,34 @@ function Welcome() {
         <div className="pt-10 flex flex-col *:self-center gap-8 text-center ">
           <h1 className="text-8xl font-semibold text-white"
           style={{
-          fontSize:"5rem", fontFamily:"'Poppins', sans-serif", color:"transparent", WebkitTextStroke:"1px white", fontWeight:"600",
+          fontSize:"10rem", fontFamily:"'Poppins', sans-serif", color:"transparent", WebkitTextStroke:"1px white", fontWeight:"600",
           }}
           >ReviewMate</h1>
           <a
             className="w-fit text-xl border-solid border-black border-[3px] ease-in duration-50 p-3 rounded-xl hover:invert hover:bg-white"
             href="/login"
+            stylel={{
+              width:"fit-content",
+              fontSize:"2rem",
+              border:"2px solid white",
+              color:"white",
+              padding: "10px 20px",
+              borderRadius: "10px",
+              textDecoration: "none",
+              backgroundColor:"transparent",
+              display: "inline-block",
+              transition: "background-color 0.3s ease",
+            }}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = "blue")}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}
+
           >
             Login
           </a>
           <a
             className="w-fit text-xl border-solid border-black border-[3px] ease-in duration-50 p-3 rounded-xl hover:invert hover:bg-white"
             href="/registration"
+            
           >
             Register
           </a>
